64870c771a689f6e993fe32e03249112d0eff420
Charles H. Leggett <charles@solarturtle.net>
2015-12-24 06:06:21 -0500

    Revert "swig is satisfied by cmake. It's too early in the morning. ;-)"
    This reverts commit 76e044288ffd4b6448d29505f14c05d9a21c3abf.

--- gr-osmosdr.rb
+++ gr-osmosdr.rb
@@ -6,6 +6,7 @@ class GrOsmosdr < Formula
   sha1 "672041a6ffa767d39ffad2432e2a13c11e3ec984"
 
   depends_on "cmake" => :build
+  depends_on "swig" => :build
   depends_on :python
   build.without? "python-deps"
   depends_on "Cheetah" => :python
