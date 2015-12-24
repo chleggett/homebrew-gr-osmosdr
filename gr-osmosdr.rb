685695eebcd1825982c81ad1f157591365cd2af9
Charles H. Leggett <charles@solarturtle.net>
2015-12-24 06:24:19 -0500

    Cleaning up from a failed revert.

--- gr-osmosdr.rb
+++ gr-osmosdr.rb
@@ -6,7 +6,6 @@ class GrOsmosdr < Formula
   sha1 "672041a6ffa767d39ffad2432e2a13c11e3ec984"
 
   depends_on "cmake" => :build
-  depends_on "swig" => :build
   depends_on :python
   build.without? "python-deps"
   depends_on "Cheetah" => :python
@@ -14,32 +13,7 @@ class GrOsmosdr < Formula
 
   def install
     mkdir "build" do
-      args = std_cmake_args
-
-      # From opencv.rb
-      python_prefix = `python-config --prefix`.strip
-      # Python is actually a library. The libpythonX.Y.dylib points to this lib, too.
-      if File.exist? "#{python_prefix}/Python"
-        # Python was compiled with --framework:
-        args << "-DPYTHON_LIBRARY='#{python_prefix}/Python'"
-        if !MacOS::CLT.installed? and python_prefix.start_with? '/System/Library'
-          # For Xcode-only systems, the headers of system's python are inside of Xcode
-          args << "-DPYTHON_INCLUDE_DIR='#{MacOS.sdk_path}/System/Library/Frameworks/Python.framework/Versions/2.7/Headers'"
-        else
-          args << "-DPYTHON_INCLUDE_DIR='#{python_prefix}/Headers'"
-        end
-      else
-        python_lib = "#{python_prefix}/lib/lib#{which_python}"
-        if File.exists? "#{python_lib}.a"
-          args << "-DPYTHON_LIBRARY='#{python_lib}.a'"
-        else
-          args << "-DPYTHON_LIBRARY='#{python_lib}.dylib'"
-        end
-        args << "-DPYTHON_INCLUDE_DIR='#{python_prefix}/include/#{which_python}'"
-      end
-      args << "-DPYTHON_PACKAGES_PATH='#{lib}/#{which_python}/site-packages'"
-
-      system "cmake", "..", *args
+      system "cmake", "..", *std_cmake_args
       system "make", "install"
     end
   end
