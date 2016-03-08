842e2fd4e8b5b7f1b7ec271496a07af7a641aa9b
Charles H. Leggett <charles@solarturtle.net>
2016-03-08 09:58:15 -0500

    updated shasum

--- gr-osmosdr.rb
+++ gr-osmosdr.rb
@@ -3,7 +3,7 @@ require "formula"
 class GrOsmosdr < Formula
   homepage "http://sdr.osmocom.org/trac/wiki/GrOsmoSDR"
   url "http://cgit.osmocom.org/gr-osmosdr/snapshot/gr-osmosdr-0.1.4.tar.gz"
-  sha1 "672041a6ffa767d39ffad2432e2a13c11e3ec984"
+  sha1 "ffc684a8255e0750390b0d4fb48421c989babc67"
 
   depends_on "cmake" => :build
   depends_on :python
